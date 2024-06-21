d665cb91e8eec4923abb931b55df3ea927af1889
Aceaks <106340449+Aceaks@users.noreply.github.com>
2024-06-21 22:32:07 +0530

    For testing on another set of images

--- gender_classify.py
+++ gender_classify.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("model_files/gender_segment1.pt")  # To load the custom model file
 
 # Predict with the model
-results = model("web_scrapped_image_data/gender/males/full_sleeves/")  # predict on the images
+results = model("web_scrapped_image_data/gender/males/half_sleeves/")  # predict on the images
